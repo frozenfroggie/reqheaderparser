4faa17ccec6c10bece7c0257a8224d285a77d6c1
Jakub Wojtyra <wojtyraj@gmail.com>
2017-03-24 12:33:26 +0000

    finishial commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 var express = require("express");
+var app = express();
+
+var ip = require("ip");
+app.get('/whoami', function(req, res) {
+    var info = {
+         'ipaddress':  req.headers['x-forwarded-for'],
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+    };
+  res.json(info);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log('Node listening on port: ' + port);
+});
