04e92133670a3323708f00d20190dd1fe16a3ace
Jakub Wojtyra <wojtyraj@gmail.com>
2017-03-24 16:09:28 +0000

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 var ip = require("ip");
-app.get('/whoami', function(req, res) {
+app.get('/', function(req, res) {
     var info = {
          'ipaddress':  req.headers['x-forwarded-for'],
          'language': req.headers["accept-language"].split(',')[0],
@@ -11,7 +11,7 @@ app.get('/whoami', function(req, res) {
   res.json(info);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log('Node listening on port: ' + port);
 });
